780d263930e38a4c771e87c8c5d0618cc13da2ab
zikhad <zikhad@protonmail.com>
2024-10-28 11:26:50 -0300

    fix(items): changed distribution chances

--- media/lua/server/Items/ZWBFDistributions.lua
+++ media/lua/server/Items/ZWBFDistributions.lua
@@ -3,67 +3,74 @@ Distributions = Distributions or {}
 local distributionTable = {
     all = {
         inventoryfemale = {
+            rolls = 2,
             items = {
-                {item = "ZWBF.Contraceptive", amount = 20},
-                {item = "ZWBF.Lactaid", amount = 10},
-                {item = "ZWBF.BreastPump", amount = 1},
-                {item = "ZWBF.BreastPump", amount = 1},
-                {item = "ZWBF.VaginalDouche_empty", amount = 5},
+                {item = "ZWBF.Contraceptive", amount = 30},
+                {item = "ZWBF.Lactaid", amount = 15},
+                {item = "ZWBF.BreastPump", amount = 10},
+                {item = "ZWBF.BreastPump", amount = 10},
+                {item = "ZWBF.VaginalDouche_empty", amount = 20},
             }
         },
         medicine = {
+            rolls = 5,
             items = {
-                {item = "ZWBF.Contraceptive", amount = 10},
-                {item = "ZWBF.Lactaid", amount = 5},
-                {item = "ZWBF.BreastPump", amount = 2},
-                {item = "ZWBF.VaginalDouche_empty", amount = 5},
+                {item = "ZWBF.Contraceptive", amount = 40},
+                {item = "ZWBF.Lactaid", amount = 10},
+                {item = "ZWBF.BreastPump", amount = 10},
+                {item = "ZWBF.VaginalDouche_empty", amount = 20},
             }
         }
     },
     bathroom = {
         counter = {
+            rolls = 5,
             items = {
-                {item = "ZWBF.Contraceptive", amount = 20},
-                {item = "ZWBF.Lactaid", amount = 5},
+                {item = "ZWBF.Contraceptive", amount = 70},
+                {item = "ZWBF.Lactaid", amount = 10},
                 {item = "ZWBF.BreastPump", amount = 10},
-                {item = "ZWBF.VaginalDouche_empty", amount = 15},
+                {item = "ZWBF.VaginalDouche_empty", amount = 30},
             }
         },
         medicine = {
+            rolls = 5,
             items = {
-                {item = "ZWBF.Contraceptive", amount = 20},
-                {item = "ZWBF.Lactaid", amount = 5},
-                {item = "ZWBF.BreastPump", amount = 5},
-                {item = "ZWBF.VaginalDouche_empty", amount = 10},
+                {item = "ZWBF.Contraceptive", amount = 60},
+                {item = "ZWBF.Lactaid", amount = 10},
+                {item = "ZWBF.BreastPump", amount = 10},
+                {item = "ZWBF.VaginalDouche_empty", amount = 15},
             }
         }
     },
     motelroomoccupied = {
         sidetable = {
+            rolls = 5,
             items = {
-                {item = "ZWBF.Contraceptive", amount = 5},
-                {item = "ZWBF.BreastPump", amount = 1},
-                {item = "ZWBF.VaginalDouche_empty", amount = 5},
+                {item = "ZWBF.Contraceptive", amount = 40},
+                {item = "ZWBF.BreastPump", amount = 10},
+                {item = "ZWBF.VaginalDouche_empty", amount = 10},
             }
         }
     },
     Bag_DoctorBag = {
         counter = {
+            rolls = 5,
             items = {
-                {item = "ZWBF.Contraceptive", amount = 20},
-                {item = "ZWBF.Lactaid", amount = 10},
-                {item = "ZWBF.BreastPump", amount = 2},
-                {item = "ZWBF.VaginalDouche_empty", amount = 5},
+                {item = "ZWBF.Contraceptive", amount = 75},
+                {item = "ZWBF.Lactaid", amount = 50},
+                {item = "ZWBF.BreastPump", amount = 20},
+                {item = "ZWBF.VaginalDouche_empty", amount = 20},
             }
         }
     },
     Bag_MedicalBag = {
         counter = {
+            rolls = 5,
             items = {
-                {item = "ZWBF.Contraceptive", amount = 10},
-                {item = "ZWBF.Lactaid", amount = 5},
-                {item = "ZWBF.BreastPump", amount = 4},
-                {item = "ZWBF.VaginalDouche_empty", amount = 5},
+                {item = "ZWBF.Contraceptive", amount = 75},
+                {item = "ZWBF.Lactaid", amount = 50},
+                {item = "ZWBF.BreastPump", amount = 40},
+                {item = "ZWBF.VaginalDouche_empty", amount = 35},
             }
         }
     }
