af0ae62babbb08a837f0a513801acf42fbd4d720
zikhad <zikhad@protonmail.com>
2025-01-06 21:50:33 -0300

    feat(ui): more code to insert tab

--- media/lua/client/ZWBF/ZWBISCharacterHPanel.lua
+++ media/lua/client/ZWBF/ZWBISCharacterHPanel.lua
@@ -6,71 +6,96 @@ local ISPanelJoypad = ISPanelJoypad
 local getSpecificPlayer = getSpecificPlayer
 local Joypad = Joypad
 local getPlayerInfoPanel = getPlayerInfoPanel
+local UIFont = UIFont
+local getTextManager = getTextManager
+local ISWindow = ISWindow
+local getCore = getCore
 
-ISCharacterHPanel = ISPanelJoypad:derive("ISCharacterHPanel")
+ISCharacterHPanel = ISPanelJoypad:derive("ISCharacterHPanel");
+
+local FONT_HGT_SMALL = getTextManager():getFontHeight(UIFont.Small)
+
 
 function ISCharacterHPanel:initialise()
-	print("ZWBF - UI - TAB - Initialize")
     ISPanelJoypad.initialise(self);
 end
 
-function ISCharacterHPanel:createChildren()
-	print("ZWBF - UI - TAB - Create Children")
-	self:setScrollChildren(true)
-    self:addScrollBars()
-end
-
-function ISCharacterHPanel:setVisible(visible)
-	print("ZWBF - UI - TAB - Set Visible")
-    self.javaObject:setVisible(visible);
-end
-
-function ISCharacterHPanel:prerender()
-	print("ZWBF - UI - TAB - Prerender")
-    ISPanelJoypad.prerender(self)
-    self:setStencilRect(0, 0, self.width, self.height)
-	self:createChildren()
-end
-
-function ISCharacterHPanel:render()
-	if not self.char:getModData() then self:clearStencilRect(); return end
-end
-
-function ISCharacterHPanel:onMouseWheel(del)
-	print("ZWBF - UI - TAB - on onMouseWheel")
-    self:setYScroll(self:getYScroll() - del * 30)
-    return true
-end
-
 function ISCharacterHPanel:new(x, y, width, height, playerNum)
-	print("ZWBF - UI - TAB - New")
-	local o = {};
+    local o = {};
     o = ISPanelJoypad:new(x, y, width, height);
     setmetatable(o, self);
     self.__index = self;
     o.playerNum = playerNum
     o.char = getSpecificPlayer(playerNum);
     o:noBackground();
-    o.textX = 20
-    o.inputX = 300
+    o.textX = 0
     o.textY = 0
-    
     ISCharacterHPanel.instance = o;
    return o;
 end
 
-function ISCharacterHPanel:ensureVisible()
-	print("ZWBF - UI - TAB - ensureVisible")
-    if not self.joyfocus then return end
-    local child = nil;
-    if not child then return end
-    local y = child:getY()
-    if y - 40 < 0 - self:getYScroll() then
-        self:setYScroll(0 - y + 40)
-    elseif y + child:getHeight() + 40 > 0 - self:getYScroll() + self:getHeight() then
-        self:setYScroll(0 - (y + child:getHeight() + 40 - self:getHeight()))
-    end
+function ISCharacterHPanel:createChildren()
+    print("ZWBG Panel - create children here")
+    self.textY = 0
+    ---
+    local image = ISSimpleImage:new(self,"media/ui/womb/normal/womb_normal_0.png")
+    self:addChild(image)
+    
+    ---
+    self:setScrollChildren(true)
+    self:addScrollBars()
 end
+
+function ISCharacterHPanel:setVisible(visible)
+    self.javaObject:setVisible(visible);
+end
+
+function ISCharacterHPanel:prerender()
+    ISPanelJoypad.prerender(self)
+    self:setStencilRect(0, 0, self.width, self.height)
+    self:createChildren()
+end
+
+function ISCharacterHPanel:addTextLine(str, textX, textY, maxTextWidth)
+    local txt = "- " .. str
+    self:drawText(txt, textX, textY, 1, 1, 1, 1, UIFont.Small)
+    local txtWidth = getTextManager():MeasureStringX(UIFont.Small, txt)
+    if txtWidth > maxTextWidth then maxTextWidth = txtWidth end
+    textY = textY + FONT_HGT_SMALL
+    return maxTextWidth, textY
+end
+
+function ISCharacterHPanel:render()
+    local textX = self.textX
+    local textY = self.textY
+    local maxTextWidth = 0
+    
+    -- TODO: components should be put in here
+    
+    maxTextWidth, textY = self:addTextLine("xpto", textX, textY, maxTextWidth)
+
+    --- calculate witdh and height of the panel
+    local widthRequired = self.textX * 2 + maxTextWidth;
+    if widthRequired > self:getWidth() then
+        self:setWidthAndParentWidth(widthRequired);
+    end
+
+    local tabHeight = self.y
+    local maxHeight = getCore():getScreenHeight() - tabHeight
+    if ISWindow and ISWindow.TitleBarHeight then maxHeight = maxHeight - ISWindow.TitleBarHeight end
+    
+    self:setHeightAndParentHeight(math.min(textY, maxHeight));
+    self:setScrollHeight(textY)
+    
+    self:clearStencilRect()
+end
+
+function ISCharacterHPanel:onMouseWheel(del)
+    self:setYScroll(self:getYScroll() - del * 30)
+    return true
+end
+
+--[[
 
 function ISCharacterHPanel:onGainJoypadFocus(joypadData)
     ISPanelJoypad.onGainJoypadFocus(self, joypadData);
@@ -103,6 +128,6 @@ end
 function ISCharacterHPanel:onJoypadDirRight()
 end
 
+]]
 
 addCharacterPageTab("HPanel", ISCharacterHPanel)
--- ISPanelJoypad.initialise(UI)
