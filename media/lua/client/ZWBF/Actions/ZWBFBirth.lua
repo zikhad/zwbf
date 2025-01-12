9f291fe47d2738081a05bf540cd9915474482d67
zikhad <zikhad@protonmail.com>
2025-01-11 22:44:15 -0300

    feat(mechanics): added birth mechanic

---
+++ media/lua/client/ZWBF/Actions/ZWBFBirth.lua
@@ -0,0 +1,59 @@
+require "TimedActions/ISBaseTimedAction"
+
+--- Localized global functions from PZ
+local ISBaseTimedAction = ISBaseTimedAction
+-- local getText = getText
+local CharacterActionAnims = CharacterActionAnims
+local triggerEvent = triggerEvent
+
+-- local Womb = require("ZWBF/ZWBFWomb")
+
+--- This file creates the Timed Action for when contraceptive is taken 
+ZWBFActionBirth = ISBaseTimedAction:derive("ZWBFActionBirth")
+    
+function ZWBFActionBirth:isValid()
+	return true
+end
+
+function ZWBFActionBirth:update()
+    -- print("ZWBFActionBirth:update()" .. tostring())
+	-- self.pregnancy:setJobDelta(self:getJobDelta())
+    self.pregnancy:setLaborProgress(self:getJobDelta())
+end
+
+function ZWBFActionBirth:start()
+	-- self.pills:setJobType(getText("ContextMenu_Take_Contraceptive"))
+	-- self.pregnancy:setJobDelta(0.0)
+	self:setActionAnim(--[[ CharacterActionAnims.TakePills]] "Yonchi_breastpump")
+    -- TODO: Add the custom animation
+	-- self:setOverrideHandModels(nil, self.pills)
+	-- self.character:playSound("Pills_A")
+end
+
+function ZWBFActionBirth:stop()
+	ISBaseTimedAction.stop(self)
+	-- self.pregnancy:setJobDelta(0.0)
+end
+
+function ZWBFActionBirth:perform()
+	-- self.pills:getContainer():setDrawDirty(true)
+	-- self.pregnancy:setJobDelta(0.0)
+	-- self.pills:Use()
+	ISBaseTimedAction.perform(self)
+    -- self.pregnancy:onBirth()
+    triggerEvent("ZWBFPregnancyBirth", self.pregnancy)
+	-- Womb:setContraceptive(true)
+end
+
+function ZWBFActionBirth:new(character, pregnancy)
+    print("ZWBFActionBirth:new()")
+	local o = setmetatable({}, self)
+	self.__index = self
+	o.character = character
+    o.pregnancy = pregnancy
+	-- o.pills = pills
+	o.maxTime = 1000
+	o.stopOnWalk = false
+	o.stopOnRun = false
+	return o
+end
