c5f2a392cb8e462cbd31b1144328e4080ec3c966
zikhad <zikhad@protonmail.com>
2025-01-20 15:35:58 -0300

    chore(mechanics): code cleanup

--- media/lua/client/ZWBF/Actions/ZWBFBirth.lua
+++ media/lua/client/ZWBF/Actions/ZWBFBirth.lua
@@ -3,12 +3,12 @@ require "TimedActions/ISBaseTimedAction"
 --- Localized global functions from PZ
 local ISBaseTimedAction = ISBaseTimedAction
 -- local getText = getText
-local CharacterActionAnims = CharacterActionAnims
+-- local CharacterActionAnims = CharacterActionAnims
 local triggerEvent = triggerEvent
 
 -- local Womb = require("ZWBF/ZWBFWomb")
 
---- This file creates the Timed Action for when contraceptive is taken 
+--- This file creates the Timed Action for when Birth occurs
 ZWBFActionBirth = ISBaseTimedAction:derive("ZWBFActionBirth")
     
 function ZWBFActionBirth:isValid()
@@ -16,46 +16,34 @@ function ZWBFActionBirth:isValid()
 end
 
 function ZWBFActionBirth:update()
-    -- print("ZWBFActionBirth:update()" .. tostring())
-	-- self.pregnancy:setJobDelta(self:getJobDelta())
     self.pregnancy:setLaborProgress(self:getJobDelta())
 	triggerEvent("ZWBFPregnancyLaborUpdate", self.pregnancy)
 end
 
 function ZWBFActionBirth:start()
-	-- self.pills:setJobType(getText("ContextMenu_Take_Contraceptive"))
-	-- self.pregnancy:setJobDelta(0.0)
-	-- TODO: Add custom animation here
 	self:setActionAnim(--[[ CharacterActionAnims.TakePills]] "Yonchi_breastpump")
-    -- TODO: Add the custom animation
-	-- self:setOverrideHandModels(nil, self.pills)
-	-- self.character:playSound("Pills_A")
 end
 
 function ZWBFActionBirth:stop()
 	ISBaseTimedAction.stop(self)
-	-- self.pregnancy:setJobDelta(0.0)
 end
 
 function ZWBFActionBirth:perform()
-	-- self.pills:getContainer():setDrawDirty(true)
-	-- self.pregnancy:setJobDelta(0.0)
-	-- self.pills:Use()
 	ISBaseTimedAction.perform(self)
-    -- self.pregnancy:onBirth()
     triggerEvent("ZWBFPregnancyBirth", self.pregnancy)
-	-- Womb:setContraceptive(true)
 end
 
-function ZWBFActionBirth:new(character, pregnancy)
+--- comment
+--- @param pregnancy any
+--- @return table
+function ZWBFActionBirth:new(pregnancy)
     print("ZWBFActionBirth:new()")
-	local o = setmetatable({}, self)
+	local instance = setmetatable({}, self)
 	self.__index = self
-	o.character = character
-    o.pregnancy = pregnancy
-	-- o.pills = pills
-	o.maxTime = 1000
-	o.stopOnWalk = false
-	o.stopOnRun = false
-	return o
+	instance.character = pregnancy.character
+    instance.pregnancy = pregnancy
+	instance.maxTime = 1000
+	instance.stopOnWalk = false
+	instance.stopOnRun = false
+	return instance
 end
