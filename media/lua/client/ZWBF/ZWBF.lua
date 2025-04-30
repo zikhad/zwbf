947423c5d8d3095de7c4f50dec7822d373249f9d
zikhad <zikhad@protonmail.com>
2025-04-30 16:30:05 -0300

    chore(code): make womb class more oop

--- media/lua/client/ZWBF/ZWBF.lua
+++ media/lua/client/ZWBF/ZWBF.lua
@@ -1,10 +1,13 @@
-local EngorgementClass = require("ZWBF/ZWBFEngorgement")
-local InventoryClass = require("ZWBF/ZWBFInventory")
+local EngorgementClass = require("ZWBF/ZWBFEngorgementClass")
+local InventoryClass = require("ZWBF/ZWBFInventoryClass")
+-- local WombClass = require("ZWBF/ZWBFWombClass")
 -- local Lactation = require("ZWBF/ZWBFLactation")
 
 local Engorgement = EngorgementClass:new()
 local Inventory = InventoryClass:new()
+-- local Womb = WombClass:new()
 
 Inventory:registerEvents()
 Engorgement:registerEvents()
+-- Womb:registerEvents()
 
